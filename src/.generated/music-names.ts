84650c3a31d32387ae3e4924d207971c79c9a23d
Arukenofu <balkenov711@gmail.com>
2026-06-21 13:59:08 +0500

    feat: add .generated to resolve netlify issues

---
+++ src/.generated/music-names.ts
@@ -0,0 +1,8 @@
+export const musicNames = {
+  "1. Chewie Melodies - Color Your Night": "/music/1. Chewie Melodies - Color Your Night.mp3",
+  "Changing Seasons -Reload-": "/music/Changing Seasons -Reload-.mp3",
+  "Color Your Night": "/music/Color Your Night.mp3",
+  "Mass Destruction -Reload-": "/music/Mass Destruction -Reload-.mp3",
+  "When The Moon's Reaching Out Stars -Reload-": "/music/When The Moon's Reaching Out Stars -Reload-.mp3",
+  "キミの記憶 -Reload-": "/music/キミの記憶 -Reload-.mp3"
+} as const;
